12ed229f78b7b00d2822965ed1297cf786a633a6
shintarou kawabe <s.kawabe2281@gmail.com>
2021-01-29 01:54:30 +0900

    :lipstick: add emotion chakra
    add emotion chakra

--- README.md
+++ README.md
@@ -1,17 +1,54 @@
-# Next.jsチュートリアルプラス以下で遊ぶ
+# Next.jsチュートリアルの復習
+## クライアントサイドのページ遷移(ナビゲーション)
+Linkコンポーネントによって、同じNext.jsアプリ内にある２つのページ間のクライアント再度での
+ナビゲーションが可能になる。
+
+クライアントサイドのナビゲーションはページ遷移がJavaScriptを用いて行われるため
+ブラウザによって行われるデフォルトのページ遷移よりも高速になる。
+
+## コード分割とプリフェッチング
+Next.js は自動的にコード分割を行うので、各ページはそのページに必要なものだけを読み込みます。
+つまり、トップのページがレンダリングされたときに、
+他のページのコードが最初からサーブされるわけではないということです。
+
+Linkコンポーネントがブラウザのビューポートに表示されると
+Next.jsは自動的にリンク先のページのコードをバックグラウンドで**プリフェッチ**する。
+リンクをクリックするときまでには、目的のページのコードはバックグラウンドで読み込み済みになる！
+
+> Next.jsのアプリ外のサイトへ遷移する場合はLinkでなく<a>タグで対応する。
+
+## アセット
+Next.jsは画像などの静的なファイルを、トップレベルのpublicディレクトリ配下で
+サーブすることができます。pagesディレクトリと同様に、puclicないのファイルは
+アプリケーションのルートから参照することができる。
+
+## メタデータ
+```tsx
+<Head>
+  <title>Create Next App</title>
+  <link rel="icon" href="/favicon.ico" />
+</Head>
+```
+`Head`タグはNextに組み込まれているReactコンポーネント。
+htmlにおける<head>タグ内の情報を変更する場合はこのHeadコンポーネントを利用する
+
+> lang属性を加えるにはカスタムのDocumentコンポーネントを使用する。
+
+
+# Next.jsチュートリアル＋以下で遊ぶ
 - TypeScript
 - ReduxToolkit
 - ChakraUI
 - Emotion
 
-# Next.js固有の型
-## 静的生成とサーバ再度レンダリング
-- getStaticProps 
-  →　GetStaticProps型を使う
-- getStaticPaths
-  →　GetStaticPaths型を使う
-- getServerSideProps
-  →　GetServerSideProps型を使う
+## Next.js固有の型
+### 静的生成とサーバーサイドレンダリング
+- getStaticProps<br>
+  →GetStaticProps型を使う<br>
+- getStaticPaths<br>
+  →GetStaticPaths型を使う<br>
+- getServerSideProps<br>
+  →GetServerSideProps型を使う<br>
 
 ```tsx
 import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
@@ -29,7 +66,7 @@ export const getServerSideProps: GetServerSideProps = async context => {
 }
 ```
 
-## APIルート
+### APIルート
 APIルートに組み込まれている型の使い方
 ```tsx
 import { NextApiRequest, NextApiResponse } from 'next'
@@ -39,7 +76,7 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 }
 ```
 
-## Appのカスタム
+### Appのカスタム
 `pages/_app,js`をtsxにして`AppProps`というビルトインの型を使用することができる。
 ```tsx
 import { AppProps } from 'next/app'
@@ -50,3 +87,33 @@ function App({ Component, pageProps }: AppProps) {
 
 export default App
 ```
+
+## index.tsのstyled-jsxからEmotionに変えてみる
+### chakraUIとEmotionをインストールしてみる
+```
+yarn add @chakra-ui/react @emotion/react @emotion/styled framer-motion
+```
+
+### Emotionの書き方
+#### styledを使う
+```tsx
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export default function Home() {
+  return (
+    <Container>
+    ...
+    </Container>
+```
+
+#### css in propを使う
+エラーになる... よくわからない
---
+++ pages/index.tsx
@@ -0,0 +1,110 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styled from '@emotion/styled'
+
+// const container = css`
+//   min-height: 100vh;
+//   padding: 0 0.5rem;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+// `
+
+const Container = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Main = styled.main`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Footer = styled.footer`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > img {
+    margin-left: 0.5rem;
+  }
+  & > a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+
+export default function Home() {
+  return (
+    <Container>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Main>
+        <h1 className="title">
+          Read <Link href="/posts/first-post">Next.js!</Link>
+        </h1>
+
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+
+        <div className="grid">
+          <a href="https://nextjs.org/docs" className="card">
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className="card">
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className="card"
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className="card"
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </Main>
+
+      <Footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </Footer>
+    </Container>
+  )
+}
