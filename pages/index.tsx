74a9cacda360ce0473c6ac384d0fdd2517591c55
shintarou kawabe <s.kawabe2281@gmail.com>
2021-01-30 01:52:09 +0900

    :lipstick: practice emotion css prop,global style..etc
    practice emotion css prop,global style..etc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,11 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react'
 import Head from 'next/head'
 import Link from 'next/link'
-import styled from '@emotion/styled'
+import Layout from '@/Layout';
 
-// const container = css`
-//   min-height: 100vh;
-//   padding: 0 0.5rem;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-// `
 
-const Container = styled.div`
+const container = css`
   min-height: 100vh;
   padding: 0 0.5rem;
   display: flex;
@@ -20,7 +14,7 @@ const Container = styled.div`
   align-items: center;
 `
 
-const Main = styled.main`
+const main = css`
   padding: 5rem 0;
   flex: 1;
   display: flex;
@@ -29,63 +23,123 @@ const Main = styled.main`
   align-items: center;
 `
 
-const Footer = styled.footer`
+const footer = css`
   width: 100%;
   height: 100px;
-  border-top: 1px solid #eaeaea;
+  border-top: 1px solid #eaeaea;y
   display: flex;
   justify-content: center;
   align-items: center;
-  & > img {
+  img {
     margin-left: 0.5rem;
   }
-  & > a {
+  a {
     display: flex;
     justify-content: center;
     align-items: center;
   }
 `
 
+const title = css `
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+  text-align: center;
+  a {
+    color: #0070f3;
+    text-decoration: none;
+  }
+  a:hover,
+  a:focus,
+  a:active {
+    text-decoration: underline;
+  }
+`
+
+const code = css`
+  background: #fafafa;
+  border-radius: 5px;
+  padding: 0.75rem;
+  font-size: 1.1rem;
+  font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+  DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+`
+
+const grid = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 800px;
+  margin-top: 3rem;
+`
+
+const card = css`
+  margin: 1rem;
+  flex-basis: 45%;
+  padding: 1.5rem;
+  text-align: left;
+  color: inherit;
+  text-decoration: none;
+  border: 1px solid #eaeaea;
+  border-radius: 10px;
+  transition: color 0.15s ease, border-color 0.15s ease;
+  &:hover,
+  &:focus,
+  &:active {
+    color: #0070f3;
+    border-color: #0070f3;
+  }
+  h3 {
+    margin: 0 0 1rem 0;
+    font-size: 1.5rem;
+  }
+  p {
+    margin: 0;
+    font-size: 1.25rem;
+    line-height: 1.5;
+  }
+`
 
 export default function Home() {
   return (
-    <Container>
+    <div css={container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Main>
-        <h1 className="title">
+      <main css={main}>
+        <h1 css={title}>
           Read <Link href="/posts/first-post">Next.js!</Link>
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <p css={css`text-align: center;`}>
+          Get started by editing <code css={code}>pages/index.js</code>
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
+        <div css={grid}>
+          <a css={card} href="https://nextjs.org/docs">
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
+          <a css={card} href="https://nextjs.org/learn">
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
+            css={card}
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
           >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
+            css={card}
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
           >
             <h3>Deploy &rarr;</h3>
             <p>
@@ -93,18 +147,19 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </Main>
+      </main>
 
-      <Footer>
+      <footer css={footer}>
         <a
+          css={card}
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Powered by
+          <img src="/vercel.svg" alt="Vercel Logo" css={css`height: 1em;`} />
         </a>
-      </Footer>
-    </Container>
+      </footer>
+    </div>
   )
 }
--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from 'component/Layout';
 
 const FirstPost = (): JSX.Element => {
   return (
